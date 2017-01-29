4ffd0bc506f15cac042416e2c4d8de16da4b3963
Kathy Coomes <kathy.coomes@marist.edu>
2017-01-29 11:47:50 -0500

    Lab3 - SQL queries - submission
    Lab3 - SQL queries - submission
    It all workds, except for that .00 thing not displaying

---
+++ Lab3- SQL queries - submission.sql
@@ -0,0 +1,19 @@
+*****Kathy Coomes - 1/29/17 - Lab3 - SQL queries- submission*****
+
+select ordNumber, totalUSD from Orders;
+
+select name, city from Agents where name = 'Smith';
+
+select pid, name, priceUSD from Products where quantity > 200100;
+
+select name, city from Customers where city = 'Duluth';
+
+select name from Agents where city not in ('New York', 'Duluth');
+
+select * from Products where (city not in ('Dallas', 'Duluth') and priceUSD >= 1.00);
+
+select * from Orders where month in ('Feb', 'May');
+
+select * from Orders where month = 'Feb' and totalUSD >= 600;
+
+select * from Orders where cid = 'c005';
